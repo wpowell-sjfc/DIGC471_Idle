173dec3a3dd757d6490d0605fc2b932a3c4813b5
wpowell-sjfc <wpowell@sjfc.edu>
2019-11-20 19:39:52 -0500

    11-20 Save files.

---
+++ Assets/Scripts/SaveLoadJson.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class SaveLoadJson : MonoBehaviour
+{
+    private static string jsonSavePath;
+
+    public static void LoadData()
+    {
+        LemonSave lemonSave = JsonUtility.FromJson<LemonSave>(File.ReadAllText(jsonSavePath));
+
+        GlobalLemonade.LemonadeCount = lemonSave.serializedLemonade;
+        GlobalCash.CashCount = lemonSave.serializedPounds;
+
+        //For testing purposes
+        Debug.Log("Lemonade: " + GlobalLemonade.LemonadeCount);
+        Debug.Log("Pounds: " + GlobalCash.CashCount);
+        LemonSave.loaded = true;
+    }
+
+    public static void SaveData()
+    {
+        //Reference
+        LemonSave lemonSave = new LemonSave();
+
+        //Lemonade
+        lemonSave.serializedLemonade = GlobalLemonade.LemonadeCount;
+        LemonSave.lemonade = lemonSave.serializedLemonade;
+
+        //Pounds
+        lemonSave.serializedPounds = GlobalCash.CashCount;
+        LemonSave.pounds = lemonSave.serializedPounds;
+
+        Debug.Log("Saving Lemonade Value: " + GlobalLemonade.LemonadeCount);
+        Debug.Log("Saving Cash Value: " + GlobalCash.CashCount);
+
+        string jsonData = JsonUtility.ToJson(lemonSave, true);
+        File.WriteAllText(jsonSavePath, jsonData);
+        Debug.Log(jsonData);
+    }
+
+    void Start()
+    {
+        jsonSavePath = Application.persistentDataPath + "/lemonadestand.json";
+        Debug.Log(jsonSavePath);
+        if (File.Exists(jsonSavePath))
+        {
+            SaveLoadJson.LoadData();
+        }
+        StartCoroutine(AutoSave());
+    }
+
+    IEnumerator AutoSave()
+    {
+        yield return new WaitForSeconds(30);
+        SaveLoadJson.SaveData();
+        StartCoroutine(AutoSave());
+    }
+}
+
+public class LemonSave
+{
+    public int serializedLemonade;
+    public int serializedPounds;
+    public static int lemonade;
+    public static int pounds;
+    public static bool loaded = false;
+}
---
+++ Assets/Scripts/SaveLoadPlayerPrefs.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using UnityEngine.UI;
+
+public class SaveLoadPlayerPrefs : MonoBehaviour
+{
+    public static void LoadData()
+    {
+        int savedLemons = PlayerPrefs.GetInt("SavedLemons");
+        GlobalLemonade.LemonadeCount = savedLemons;
+        int savedCash = PlayerPrefs.GetInt("SavedCash");
+        GlobalCash.CashCount = savedCash;
+
+        //For testing purposes
+        Debug.Log("Lemonade: " + GlobalLemonade.LemonadeCount);
+        Debug.Log("Pounds: " + GlobalCash.CashCount);
+    }
+
+    public static void SaveData()
+    {
+        PlayerPrefs.SetInt("SavedLemons", GlobalLemonade.LemonadeCount);
+        PlayerPrefs.SetInt("SavedCash", GlobalCash.CashCount);
+
+        Debug.Log("Saving Lemonade Value: " + GlobalLemonade.LemonadeCount);
+        Debug.Log("Saving Cash Value: " + GlobalCash.CashCount);
+    }
+
+    void Start()
+    {
+        SaveLoadPlayerPrefs.LoadData();
+    }
+}
